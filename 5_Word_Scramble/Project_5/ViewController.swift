517edd1d59cdbf5905538ad61807e6ba38385d1e
Gerardo Gordillo <33912419+Jerriodas@users.noreply.github.com>
2019-05-24 23:29:37 -0500

    Add files via upload
    An anagram game while learning about closures and booleans.

---
+++ 5_Word_Scramble/Project_5/ViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ViewController.swift
+//  Project_5
+//
+//  Created by Jerry Gordillo on 4/22/19.
+//  Copyright © 2019 JerriodasCorp. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        // Do any additional setup after loading the view.
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt"){
+            if let startWords = try? String(contentsOfFile: startWordsPath){
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }else {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+    }
+    
+    func startGame(){
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        title = allWords[0]
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    @objc func promptForAnswer(){
+        let ac = UIAlertController(title: "Enter Answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] (action: UIAlertAction) in
+            let answer = ac.textFields![0]
+            self.submit(answer: answer.text!)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(answer: String){
+        
+        let lowerAnswer = answer.lowercased()
+        
+        if isPossible(word: lowerAnswer){
+            if isOriginal(word: lowerAnswer){
+                if isReal(word: lowerAnswer){
+                    usedWords.insert(answer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                }
+                else{
+                    showErrorMessage(title: "Word not recognised", message: "You can't just make them up, you know!")
+                }
+            } else{
+                showErrorMessage(title: "Word used already", message: "Be more original!")
+            }
+        } else{
+            showErrorMessage(title: "Word not possible", message: "You can't spell that word from '\(title!.lowercased())'!")
+        }
+    }
+    
+    func isPossible(word: String) -> Bool {
+        
+        var tempWord = title!.lowercased()
+        
+        for letter in word{
+            if let pos = tempWord.range(of: String(letter)){
+                tempWord.remove(at: pos.lowerBound)
+            } else{
+                return false
+            }
+        }
+        return true
+    }
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    func isReal(word: String) -> Bool {
+        if word.utf16.count <= 1{
+            return false
+        }else{
+        let checker = UITextChecker()
+        let range = NSMakeRange(0, word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+        }
+    }
+    
+    func showErrorMessage(title errorTitle: String, message errorMessage: String){
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+
+
+}
+
